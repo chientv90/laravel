fe4dc74b344da8b47a64928aeea33d0a6bb45483
ChienTV <chien.tran@ntq-solution.com.vn>
2020-05-08 14:04:16 +0700

    Create data test for User model

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', 'UserController@index')->name('user.index');
+Route::get('login', 'UserController@login')->name('login');
+Route::get('register', 'UserController@register')->name('register');
+
+Route::resource('users', 'UserController');
