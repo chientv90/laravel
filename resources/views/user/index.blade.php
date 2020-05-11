828ade83e70f0896bba31c8fa8bb8e7d7a908ddb
ChienTV <chien.tran@ntq-solution.com.vn>
2020-05-11 15:07:38 +0700

    Create app layout

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,9 +1,17 @@
+@extends('layouts.app')
+
 <table>
     @foreach($activedUsers as $activedUser)
         <tr>
             <td>{{ $activedUser->id }}</td>
             <td>{{ $activedUser->name }}</td>
             <td>{{ $activedUser->email }}</td>
+            <td>{{ $activedUser->emailVerifiedAt }}</td>
+            <td>{{ $activedUser->email_verified_at }}</td>
+{{--            <td>{{ $activedUser->password }}</td>--}}
+            <td>{{ $activedUser->rememberToken }}</td>
+            <td>{{ $activedUser->createdAt }}</td>
+            <td>{{ $activedUser->updatedAt }}</td>
         </tr>
     @endforeach
 </table>
