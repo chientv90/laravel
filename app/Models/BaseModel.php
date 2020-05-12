45392c4b818356305be899c4a469f8c1178a4bb5
ChienTV <chien.tran@ntq-solution.com.vn>
2020-05-12 11:25:50 +0700

    Remove UseCamelCaseAttributes trait

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use App\Models\Traits\UseCamelCaseAttributes;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
-    use UseCamelCaseAttributes;
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
-use App\Models\Traits\UseCamelCaseAttributes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable, UseCamelCaseAttributes;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
